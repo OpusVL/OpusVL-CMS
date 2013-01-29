d617d3a755423e00124cf780467e93a3ebc38f2f
Brad <brad@geeksware.net>
2013-01-29 15:27:37 +0000

    Added new fields to accomodate default attributes

--- lib/OpusVL/CMS/Schema/Result/DefaultAttribute.pm
+++ lib/OpusVL/CMS/Schema/Result/DefaultAttribute.pm
@@ -42,13 +42,13 @@ __PACKAGE__->table("default_attributes");
   is_nullable: 0
   sequence: 'default_attributes_id_seq'
 
-=head2 name
+=head2 code
 
   data_type: 'text'
   is_nullable: 0
   original: {data_type => "varchar"}
 
-=head2 code
+=head2 name
 
   data_type: 'text'
   is_nullable: 0
@@ -57,7 +57,19 @@ __PACKAGE__->table("default_attributes");
 =head2 value
 
   data_type: 'text'
+  is_nullable: 1
+  original: {data_type => "varchar"}
+
+=head2 type
+
+  data_type: 'text'
   is_nullable: 0
+  original: {data_type => "varchar"}
+
+=head2 field_type
+
+  data_type: 'text'
+  is_nullable: 1
   original: {data_type => "varchar"}
 
 =cut
@@ -70,13 +82,13 @@ __PACKAGE__->add_columns(
     is_nullable       => 0,
     sequence          => "default_attributes_id_seq",
   },
-  "name",
+  "code",
   {
     data_type   => "text",
     is_nullable => 0,
     original    => { data_type => "varchar" },
   },
-  "code",
+  "name",
   {
     data_type   => "text",
     is_nullable => 0,
@@ -85,7 +97,19 @@ __PACKAGE__->add_columns(
   "value",
   {
     data_type   => "text",
+    is_nullable => 1,
+    original    => { data_type => "varchar" },
+  },
+  "type",
+  {
+    data_type   => "text",
     is_nullable => 0,
+    original    => { data_type => "varchar" },
+  },
+  "field_type",
+  {
+    data_type   => "text",
+    is_nullable => 1,
     original    => { data_type => "varchar" },
   },
 );
@@ -102,9 +126,26 @@ __PACKAGE__->add_columns(
 
 __PACKAGE__->set_primary_key("id");
 
+=head1 RELATIONS
 
-# Created by DBIx::Class::Schema::Loader v0.07017 @ 2012-10-24 11:27:29
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:7xvsD9THDU3R2kKnKbmdNg
+=head2 default_attribute_values
+
+Type: has_many
+
+Related object: L<OpusVL::CMS::Schema::Result::DefaultAttributeValue>
+
+=cut
+
+__PACKAGE__->has_many(
+  "values",
+  "OpusVL::CMS::Schema::Result::DefaultAttributeValue",
+  { "foreign.field_id" => "self.id" },
+  { cascade_copy => 0, cascade_delete => 0 },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07017 @ 2013-01-29 14:43:25
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:rBa505qLqPQIK61SWlUCrQ
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
