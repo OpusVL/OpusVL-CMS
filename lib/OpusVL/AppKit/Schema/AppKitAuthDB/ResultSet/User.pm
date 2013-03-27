f59983c9efc8c269a25e75d32909fc56a1e9c24a
Colin Newell <colin.newell@gmail.com>
2013-03-27 09:51:37 +0000

    Setup a standard deployment script that will deploy a standard initial database.

---
+++ lib/OpusVL/AppKit/Schema/AppKitAuthDB/ResultSet/User.pm
@@ -0,0 +1,33 @@
+package OpusVL::AppKit::Schema::AppKitAuthDB::ResultSet::User;
+
+use Moose;
+extends 'DBIx::Class::ResultSet';
+
+sub initdb
+{
+    my $self = shift;
+
+}
+
+1;
+
+=head1 NAME
+
+OpusVL::AppKit::Schema::AppKitAuthDB::ResultSet::User
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head2 initdb
+
+=head1 ATTRIBUTES
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2013 OpusVL.
+
+This software is licensed according to the "IP Assignment Schedule" provided with the development project.
+
+=cut
