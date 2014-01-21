c658bc8ca2563c594033b92d94bd142a267385b9
Colin Newell <colin.newell@gmail.com>
2014-01-21 12:11:06 +0000

    Tracker 8005: Added a simple search method for the aliases so that we can deal with ambiguity

---
+++ lib/OpusVL/CMS/Schema/ResultSet/Alias.pm
@@ -0,0 +1,26 @@
+package OpusVL::CMS::Schema::ResultSet::Alias;
+
+use Moose;
+use namespace::autoclean;
+use MooseX::NonMoose;
+extends 'DBIx::Class::ResultSet';
+
+sub BUILDARGS { $_[2] };
+
+sub find_redirect
+{
+    my $self = shift;
+    my $site = shift;
+    my $url = shift;
+    my $alias = $self->search({
+        'me.url' => '/'.$url,
+        'page.site' => $site->id,
+    }, {
+        join => ['page'],
+    })->first;
+    return $alias;
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
