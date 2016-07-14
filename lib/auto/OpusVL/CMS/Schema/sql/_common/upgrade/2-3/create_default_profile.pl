2c86873a4e210f487457e85f46a7be0d2d71972a
Colin Newell <colin@opusvl.com>
2016-07-14 16:15:12 +0100

    Created migration script from old global elements

---
+++ lib/auto/OpusVL/CMS/Schema/sql/_common/upgrade/2-3/create_default_profile.pl
@@ -0,0 +1,31 @@
+sub {
+    my $schema = shift;
+    my $sites = $schema->resultset('Site');
+    my $elements = $schema->resultset('Element');
+    my $assets = $schema->resultset('Asset');
+    my $templates = $schema->resultset('Template');
+    my $profile_info = {
+        name => 'Default Profile',
+        template => 1,
+    };
+    die 'Default profile already exists' if $sites->search($profile_info)->count > 0;
+    my $profile = $sites->create($profile_info);
+    my $a = $assets->search({ global => 1 })->search_related('site');
+    my $e = $elements->search({ global => 1 })->search_related('site');
+    my $t = $templates->search({ global => 1 })->search_related('site');
+    my $sites = $a->union($e)->union($t);
+    my @users = $sites->search_related('sites_users')->get_column('user_id')->all;
+    my @updates = map { { user_id => $_ } } @users;
+    # ensure the users that previously access still do.
+    # since we're grouping up the elements from various sites this may
+    # not be precise.
+    for my $user (@updates)
+    {
+        $profile->create_related('sites_users', $user);
+    }
+    $sites->update({ profile_site => $profile->id });
+    for my $rs ($assets, $elements, $templates)
+    {
+        $rs->search({ global => 1 })->update({ site => $profile->id });
+    }
+};
