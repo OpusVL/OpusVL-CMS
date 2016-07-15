22f36cee7392991e92e03f726e19874af6886783
Colin Newell <colin@opusvl.com>
2016-07-15 09:54:51 +0100

    Added DH files for removing the table

---
+++ lib/auto/OpusVL/CMS/Schema/sql/PostgreSQL/upgrade/4-5/001-auto.sql
@@ -0,0 +1,12 @@
+-- Convert schema '/home/colin/git/OpusVL/FB11-CMS/OpusVL-CMS/lib/auto/OpusVL/CMS/Schema/sql/_source/deploy/4/001-auto.yml' to '/home/colin/git/OpusVL/FB11-CMS/OpusVL-CMS/lib/auto/OpusVL/CMS/Schema/sql/_source/deploy/5/001-auto.yml':;
+
+;
+BEGIN;
+
+;
+DROP TABLE plugins CASCADE;
+
+;
+
+COMMIT;
+
