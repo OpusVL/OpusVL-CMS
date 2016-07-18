ab57fff91fdccd22ca77f0692c5d86c20fe1097a
Colin Newell <colin@opusvl.com>
2016-07-18 08:33:28 +0100

    Various methods to help us add profile attributes

--- lib/OpusVL/CMS/Schema/ResultSet/AttachmentAttributeDetail.pm
+++ lib/OpusVL/CMS/Schema/ResultSet/AttachmentAttributeDetail.pm
@@ -2,8 +2,12 @@ package OpusVL::CMS::Schema::ResultSet::AttachmentAttributeDetail;
 our $VERSION = '5';
 
 use Moose;
+use MooseX::NonMoose;
 extends 'DBIx::Class::ResultSet';
+with 'OpusVL::CMS::Roles::ResultSetFilter' => { field => 'code' };
 __PACKAGE__->load_components(qw{Helper::ResultSet::SetOperations});
+
+sub BUILDARGS { $_[2] } # ::RS::new() expects my ($class, $rsrc, $args) = @_
 
 sub active
 {
--- lib/OpusVL/CMS/Schema/ResultSet/PageAttributeDetail.pm
+++ lib/OpusVL/CMS/Schema/ResultSet/PageAttributeDetail.pm
@@ -2,15 +2,17 @@ package OpusVL::CMS::Schema::ResultSet::PageAttributeDetail;
 our $VERSION = '5';
 
 use Moose;
+use MooseX::NonMoose;
 extends 'DBIx::Class::ResultSet';
 __PACKAGE__->load_components(qw{Helper::ResultSet::SetOperations});
+with 'OpusVL::CMS::Roles::ResultSetFilter' => { field => 'code' };
+sub BUILDARGS { $_[2] } # ::RS::new() expects my ($class, $rsrc, $args) = @_
 
 sub active
 {
     my $self = shift;
     return $self->search({ active => 1 });
 }
-
 
 1;
 
