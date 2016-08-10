d09ad7a3990454f1b30d612f221d30a82dd4b015
Alastair McGowan-Douglas <alastair.mcgowan@opusvl.com>
2016-08-10 13:36:05 +0100

    Add empty SQL for final fixing-SQL release

---
+++ lib/auto/OpusVL/CMS/Schema/sql/SQLite/upgrade/19-20/001-auto.sql
@@ -0,0 +1,5 @@
+-- Convert schema '/opt/cms/OpusVL-CMS/lib/auto/OpusVL/CMS/Schema/sql/_source/deploy/19/001-auto.yml' to '/opt/cms/OpusVL-CMS/lib/auto/OpusVL/CMS/Schema/sql/_source/deploy/20/001-auto.yml':;
+
+;
+-- No differences found;
+
