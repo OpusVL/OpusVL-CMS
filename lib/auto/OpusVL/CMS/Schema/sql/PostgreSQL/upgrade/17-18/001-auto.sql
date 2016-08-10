dd08785bf76cfd2005598fb5bf3c738ff89bfb28
Alastair McGowan-Douglas <alastair.mcgowan@opusvl.com>
2016-08-10 10:59:16 +0100

    Added SQL for 16-17, plus 17-18 so I can re-release

---
+++ lib/auto/OpusVL/CMS/Schema/sql/PostgreSQL/upgrade/17-18/001-auto.sql
@@ -0,0 +1,5 @@
+-- Convert schema '/opt/cms/OpusVL-CMS/lib/auto/OpusVL/CMS/Schema/sql/_source/deploy/17/001-auto.yml' to '/opt/cms/OpusVL-CMS/lib/auto/OpusVL/CMS/Schema/sql/_source/deploy/18/001-auto.yml':;
+
+;
+-- No differences found;
+
