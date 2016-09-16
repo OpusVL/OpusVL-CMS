287c0506cbd043905fd2494adbc98ca612ce574f
Alastair McGowan-Douglas <alastair.mcgowan@opusvl.com>
2016-09-16 15:54:31 +0100

    DH files

---
+++ lib/auto/OpusVL/CMS/Schema/sql/MySQL/upgrade/27-28/001-auto.sql
@@ -0,0 +1,5 @@
+-- Convert schema '/opt/cms/OpusVL-CMS/lib/auto/OpusVL/CMS/Schema/sql/_source/deploy/27/001-auto.yml' to '/opt/cms/OpusVL-CMS/lib/auto/OpusVL/CMS/Schema/sql/_source/deploy/28/001-auto.yml':;
+
+;
+-- No differences found;
+
