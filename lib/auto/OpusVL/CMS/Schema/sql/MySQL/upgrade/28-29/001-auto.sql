a21b8bd0a87b7b1de137f47f76b8a4f8013dd4f4
Alastair McGowan-Douglas <alastair.mcgowan@opusvl.com>
2016-09-26 16:49:20 +0100

    DH files ... probably should be avoiding these

---
+++ lib/auto/OpusVL/CMS/Schema/sql/MySQL/upgrade/28-29/001-auto.sql
@@ -0,0 +1,5 @@
+-- Convert schema '/opt/cms/OpusVL-CMS/lib/auto/OpusVL/CMS/Schema/sql/_source/deploy/28/001-auto.yml' to '/opt/cms/OpusVL-CMS/lib/auto/OpusVL/CMS/Schema/sql/_source/deploy/29/001-auto.yml':;
+
+;
+-- No differences found;
+
