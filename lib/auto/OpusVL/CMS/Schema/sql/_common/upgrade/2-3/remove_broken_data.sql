55002bfa8934634b78b1b0e82617dd1471e9d47b
Alastair McGowan-Douglas <alastair.mcgowan@opusvl.com>
2016-10-07 09:56:39 +0100

    remove other broken data

--- lib/auto/OpusVL/CMS/Schema/sql/_common/upgrade/2-3/remove_broken_data.sql
+++ lib/auto/OpusVL/CMS/Schema/sql/_common/upgrade/2-3/remove_broken_data.sql
@@ -6,5 +6,28 @@ where id in (
     inner join page_attribute_data d on d.field_id = f.id
     inner join pages p on d.page_id = p.id
     inner join sites s on s.id = p.site
+    where f.site_id <> s.id and f.site_id <> s.profile_site
+);
+
+delete
+from asset_attribute_data
+where id in (
+    select d.id
+    from asset_attribute_details f
+    inner join asset_attribute_data d on d.field_id = f.id
+    inner join assets a on d.asset_id = a.id
+    inner join sites s on s.id = a.site
+    where f.site_id <> s.id and f.site_id <> s.profile_site
+);
+
+delete
+from asset_attribute_data
+where id in (
+    select d.id
+    from attachment_attribute_details f
+    inner join attachment_attribute_data d on d.field_id = f.id
+    inner join attachments a on d.attachment_id = a.id
+    inner join pages p on a.page_id = p.id
+    inner join sites s on s.id = p.site
     where f.site_id <> p.site and f.site_id <> s.profile_site
 );
