bffbebe0eb6259ebac4fe7b752d798b1000899af
Alastair McGowan-Douglas <alastair.mcgowan@opusvl.com>
2017-06-09 11:30:43 +0100

    Unfuck migration 36-37

--- lib/auto/OpusVL/CMS/Schema/sql/PostgreSQL/upgrade/36-37/001-auto.sql
+++ lib/auto/OpusVL/CMS/Schema/sql/PostgreSQL/upgrade/36-37/001-auto.sql
@@ -1,73 +1,10 @@
--- Convert schema 'lib/auto/OpusVL/CMS/Schema/sql/_source/deploy/36/001-auto.yml' to 'lib/auto/OpusVL/CMS/Schema/sql/_source/deploy/37/001-auto.yml':;
+-- Convert schema '/opt/local/cms/OpusVL-CMS/lib/auto/OpusVL/CMS/Schema/sql/_source/deploy/36/001-auto.yml' to '/opt/local/cms/OpusVL-CMS/lib/auto/OpusVL/CMS/Schema/sql/_source/deploy/37/001-auto.yml':;
 
 ;
 BEGIN;
 
 ;
-CREATE TABLE "default_attribute_values" (
-  "id" serial NOT NULL,
-  "field_id" integer NOT NULL,
-  "value" text NOT NULL,
-  PRIMARY KEY ("id")
-);
-CREATE INDEX "default_attribute_values_idx_field_id" on "default_attribute_values" ("field_id");
-
-;
-CREATE TABLE "default_attributes" (
-  "id" serial NOT NULL,
-  "code" text NOT NULL,
-  "name" text NOT NULL,
-  "value" text,
-  "type" text NOT NULL,
-  "field_type" text,
-  PRIMARY KEY ("id")
-);
-
-;
-CREATE TABLE "plugins" (
-  "id" serial NOT NULL,
-  "name" text NOT NULL,
-  "action" text NOT NULL,
-  "code" text NOT NULL,
-  "author" text NOT NULL,
-  "description" text NOT NULL,
-  "status" text DEFAULT 'disabled' NOT NULL,
-  PRIMARY KEY ("id")
-);
-
-;
-ALTER TABLE "default_attribute_values" ADD CONSTRAINT "default_attribute_values_fk_field_id" FOREIGN KEY ("field_id")
-  REFERENCES "default_attributes" ("id") ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE;
-
-;
-ALTER TABLE aclfeature DROP COLUMN feature_description;
-
-;
-ALTER TABLE forms DROP CONSTRAINT form_name_site_id;
-
-;
-ALTER TABLE forms ADD CONSTRAINT form_name_site_id UNIQUE (name, site_id);
-
-;
-ALTER TABLE pages DROP CONSTRAINT pages_url_site;
-
-;
-ALTER TABLE pages ADD CONSTRAINT pages_url_site UNIQUE (url, site);
-
-;
 ALTER TABLE site_attributes ADD COLUMN description text;
-
-;
-ALTER TABLE templates DROP CONSTRAINT templates_name_site;
-
-;
-ALTER TABLE templates ADD CONSTRAINT templates_name_site UNIQUE (name, site);
-
-;
-ALTER TABLE users DROP CONSTRAINT user_index;
-
-;
-ALTER TABLE users ADD CONSTRAINT user_index UNIQUE (username);
 
 ;
 
--- lib/auto/OpusVL/CMS/Schema/sql/SQLite/upgrade/36-37/001-auto.sql
+++ lib/auto/OpusVL/CMS/Schema/sql/SQLite/upgrade/36-37/001-auto.sql
@@ -1,90 +1,10 @@
--- Convert schema 'lib/auto/OpusVL/CMS/Schema/sql/_source/deploy/36/001-auto.yml' to 'lib/auto/OpusVL/CMS/Schema/sql/_source/deploy/37/001-auto.yml':;
+-- Convert schema '/opt/local/cms/OpusVL-CMS/lib/auto/OpusVL/CMS/Schema/sql/_source/deploy/36/001-auto.yml' to '/opt/local/cms/OpusVL-CMS/lib/auto/OpusVL/CMS/Schema/sql/_source/deploy/37/001-auto.yml':;
 
 ;
 BEGIN;
 
 ;
-CREATE TABLE default_attribute_values (
-  id INTEGER PRIMARY KEY NOT NULL,
-  field_id integer NOT NULL,
-  value text NOT NULL,
-  FOREIGN KEY (field_id) REFERENCES default_attributes(id) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-;
-CREATE INDEX default_attribute_values_idx_field_id ON default_attribute_values (field_id);
-
-;
-CREATE TABLE default_attributes (
-  id INTEGER PRIMARY KEY NOT NULL,
-  code text NOT NULL,
-  name text NOT NULL,
-  value text,
-  type text NOT NULL,
-  field_type text
-);
-
-;
-CREATE TABLE plugins (
-  id INTEGER PRIMARY KEY NOT NULL,
-  name text NOT NULL,
-  action text NOT NULL,
-  code text NOT NULL,
-  author text NOT NULL,
-  description text NOT NULL,
-  status text NOT NULL DEFAULT 'disabled'
-);
-
-;
-CREATE TEMPORARY TABLE aclfeature_temp_alter (
-  id INTEGER PRIMARY KEY NOT NULL,
-  feature text NOT NULL
-);
-
-;
-INSERT INTO aclfeature_temp_alter( id, feature) SELECT id, feature FROM aclfeature;
-
-;
-DROP TABLE aclfeature;
-
-;
-CREATE TABLE aclfeature (
-  id INTEGER PRIMARY KEY NOT NULL,
-  feature text NOT NULL
-);
-
-;
-INSERT INTO aclfeature SELECT id, feature FROM aclfeature_temp_alter;
-
-;
-DROP TABLE aclfeature_temp_alter;
-
-;
-DROP INDEX form_name_site_id;
-
-;
-CREATE UNIQUE INDEX form_name_site_id02 ON forms (name, site_id);
-
-;
-DROP INDEX pages_url_site;
-
-;
-CREATE UNIQUE INDEX pages_url_site02 ON pages (url, site);
-
-;
 ALTER TABLE site_attributes ADD COLUMN description text;
-
-;
-DROP INDEX templates_name_site;
-
-;
-CREATE UNIQUE INDEX templates_name_site02 ON templates (name, site);
-
-;
-DROP INDEX user_index;
-
-;
-CREATE UNIQUE INDEX user_index02 ON users (username);
 
 ;
 
